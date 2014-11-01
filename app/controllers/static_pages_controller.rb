91ac04037ba7d1a9744e568a336d2cd11bb24d64
doutatsu <doutatsu@me.com>
2014-11-01 14:57:17 +0000

    try pi_piper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
+require 'pi_piper'
+include PiPiper
+
 class StaticPagesController < ApplicationController
   def index
+    sensor_status = false
+    
+    if :pin => 17, :goes => :high do
+      sensor_status = true
+    else
+      sensor_status = false
+    end
   end
   def settings
   end
